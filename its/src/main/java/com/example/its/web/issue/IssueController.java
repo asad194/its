0ef3ed4eb18a144f003a8f4572fe22271fc69ad9
admin <asdddd194@gmail.com>
2023-06-17 12:57:36 +0900

    0617

--- its/src/main/java/com/example/its/web/issue/IssueController.java
+++ its/src/main/java/com/example/its/web/issue/IssueController.java
@@ -2,22 +2,41 @@ package com.example.its.web.issue;
 
 import com.example.its.domain.issue.IssueEntity;
 import com.example.its.domain.issue.IssueService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/issues")
+@RequiredArgsConstructor
 public class IssueController {
 
-    private final IssueService issueService = new IssueService();
+    private final IssueService issueService;
 
     // GET /issues
-    @GetMapping("/issues")
+    @GetMapping
     public String showList(Model model) {
         model.addAttribute("issueList", issueService.findAll());
         return "issues/list";
+    }
+
+    @GetMapping("/creationForm")
+    public String showCreationForm(@ModelAttribute  IssueForm form) {
+        return "issues/creationForm";
+    }
+
+    // POST /issues
+    @PostMapping
+    public String create(IssueForm form, Model model) {
+        issueService.create(form.getSummary(), form.getDescription());
+
+        return showList(model); // TODO リロードボタン対策要
     }
 
 }
